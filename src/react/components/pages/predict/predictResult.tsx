3ad87a29427a4d4b3f57a64f029f469ad53f23ab
hieutran02 <hieu.tran@mrisoftware.com>
2022-07-12 14:03:54 -0400

    realign
    Signed-off-by: hieutran02 <hieu.tran@mrisoftware.com>

--- src/react/components/pages/predict/predictResult.tsx
+++ src/react/components/pages/predict/predictResult.tsx
@@ -300,25 +300,25 @@ export default class PredictResult extends React.Component<IPredictResultProps, 
     OBJtoXML = (obj) => {
         var xml = '';
         for (var prop in obj) { 
-          console.log(prop + '\n');
-          xml += obj[prop] instanceof Array ? '' : "<" + prop + ">";
-          if (obj[prop] instanceof Array) {
-            for (var array in obj[prop]) {
-              xml += "<" + prop + ">";
-              xml += this.OBJtoXML(new Object(obj[prop][array]));
-              xml += "</" + prop + ">";
+            console.log(prop + '\n');
+            xml += obj[prop] instanceof Array ? '' : "<" + prop + ">";
+            if (obj[prop] instanceof Array) {
+                for (var array in obj[prop]) {
+                xml += "<" + prop + ">";
+                xml += this.OBJtoXML(new Object(obj[prop][array]));
+                xml += "</" + prop + ">";
+                }
+            } else if (typeof obj[prop] == "object") {
+                xml += this.OBJtoXML(new Object(obj[prop]));
+            } else {
+                xml += obj[prop];
             }
-          } else if (typeof obj[prop] == "object") {
-            xml += this.OBJtoXML(new Object(obj[prop]));
-          } else {
-            xml += obj[prop];
-          }
-          xml += obj[prop] instanceof Array ? '' : "</" + prop + ">";
+            xml += obj[prop] instanceof Array ? '' : "</" + prop + ">";
         }
         //delete the index in the output file while exporting
         var xml = xml.replace(/<\/?[0-9]{1,}>/g, '');
         return xml
-      }
+    }
 
     triggerXMLDownload = () =>{
         const {analyzeResult} = this.props;
